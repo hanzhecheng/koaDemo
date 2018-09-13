d4a08acb140999843ebb7005c23ef77b30cd36db
hanzhecheng <1119216543@qq.com>
2018-09-13 20:15:54 +0800

    增加oracle调用例子

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,5 @@
 const query = require("../utils/mysql");
+const oracle_query = require("../utils/oracle")
 class User {
     constructor() {
 
@@ -11,9 +12,18 @@ class User {
         })
         ctx.body = result;
     }
+    static async GetOuInfo(ctx) {
+        let result = await oracle_query("select * from t_acl_ou").then(res => {
+            return res
+        }).catch(err => {
+            return err;
+        })
+        ctx.body = result;
+    }
     static async GetUserByID(ctx) {
         console.log(ctx.query);
         ctx.body = "测试" + ctx.query;
     }
+    
 }
 module.exports = User;
