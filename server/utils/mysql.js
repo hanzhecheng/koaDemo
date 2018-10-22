2c4bc1be48c0458495ab68b5b7855167317b2015
hanzhecheng <1119216543@qq.com>
2018-10-22 18:50:16 +0800

    修改mysql，增加保存方法（包含事务）

--- server/utils/mysql.js
+++ server/utils/mysql.js
@@ -6,24 +6,56 @@ const pool = mysql.createPool({
     database: "testdb",
     port: 3306
 })
-
-let query = function (sql, values) {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) {
-                return reject(err);
-            } else {
-                conn.query(sql, values, (err, result, field) => {
-                    if (err) {
-                        return reject(err);
-                    } else {
-                        conn.release();
-                        return resolve(result);
-                    }
-                })
-            }
+const mysqloperator = {
+    query: function (sql, values) {
+        return new Promise((resolve, reject) => {
+            pool.getConnection((err, conn) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    conn.query(sql, values, (err, result, field) => {
+                        if (err) {
+                            return reject(err);
+                        } else {
+                            conn.release();
+                            return resolve(result);
+                        }
+                    })
+                }
+            })
         })
-    })
+    },
+    insert: function (sql, params) {
+        return new Promise((resolve, reject) => {
+            pool.getConnection((connerr, connection) => {
+                if (connerr) {
+                    return reject(connerr);
+                } else {
+                    connection.beginTransaction(function (err) {
+                        if (err) { throw err; }
+                        connection.query(sql, params, function (error, results, fields) {
+                            if (error) {
+                                return connection.rollback(function () {
+                                    reject(error);
+                                });
+                            }
+                            connection.commit(function (err) {
+                                if (err) {
+                                    return connection.rollback(function () {
+                                        reject(err);
+                                    });
+                                }
+                                connection.release()
+                                resolve(results);
+                            });
+                        });
+                    });
+                }
+            })
+
+        })
+    }
 }
 
-module.exports = query;
+
+module.exports = mysqloperator;
